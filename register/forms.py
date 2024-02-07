1ee3ee1d629236469831d925493ee6f98c504014
Katarzyna Ogiela <Katarzyna.ogiela96@gmail.com>
2024-02-07 20:07:36 +0100

    commit

--- register/forms.py
+++ register/forms.py
@@ -10,5 +10,5 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['email','username','password1','password2']
 
