0d8006a746bf95a171095a9ab6f6b92b3685ca1c
spiwik98 <bzalewa@noreply.github.com>
2024-05-19 18:27:56 +0200

    Commit

--- EatNow/settings.py
+++ EatNow/settings.py
@@ -135,4 +135,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles')
+    ]
