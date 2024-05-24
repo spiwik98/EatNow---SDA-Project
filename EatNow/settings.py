154415888e9fed45022b074dbfe71702a37cea6b
spiwik98 <bzalewa@noreply.github.com>
2024-05-24 19:03:33 +0200

    pushing to render

--- EatNow/settings.py
+++ EatNow/settings.py
@@ -82,8 +82,8 @@ else:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'eatnow2',
         }
     }
 
